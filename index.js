434446a775a2c61a27a26991ef0a77cc2a7a4837
katiiisha <mendeleevakate2002@gmail.com>
2023-03-28 14:19:12 +0300

    added fcn RenderTask
    добавила функцию, которая выводит задачи на страницу

--- index.js
+++ index.js
@@ -18,6 +18,33 @@ btn.addEventListener('click', () => {
     task.id = new Date().getTime()
     task.done = false;
     addTask(task);
+    RenderTask();
+    add.value = '';
 })
+const RenderTask = (task) => { 
+    
+    ul.innerHTML = '';
+    //добавляем задачу на страницу
+    tasks.forEach(task => { 
+        
+        //создаем элементы 
+        const li = document.createElement('li');
+        const inp = document.createElement('input');
+        const btn_del = document.createElement('button');
+        const checkbox = document.createElement('input');
 
-console.log(tasks);
+        // задаем клаасы элементам 
+        li.className = 'li';
+        inp.className = 'task';
+        btn_del.className = 'btn_del';
+        checkbox.className = 'check_input';
+        checkbox.type= 'checkbox'
+        
+
+        // добавляем элементы в html разметку
+        li.append(inp, btn_del, checkbox);
+        ul.append(li);
+        inp.value = task.text
+        btn_del.innerHTML = '<img src="/image/delete.svg">'
+    })
+}
